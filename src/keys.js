0317d94b5a0c7af2788a79a8daba53b3085d454d
jrojimenes <rodrigo920215@gmail.com>
2019-11-02 08:19:17 -0600

    Update keys.js

--- src/keys.js
+++ src/keys.js
@@ -3,8 +3,8 @@ module.exports = {
     database: {
         connectionLimit: 10,
         host    : 'localhost',
-        user    : 'architecture',
-        password: 'D3v3L0p',
+        user    : 'root',
+        password: 'D3v3L0pB1g$m1l3',
         database: 'DB_BIGSMILE'
     }
 };
